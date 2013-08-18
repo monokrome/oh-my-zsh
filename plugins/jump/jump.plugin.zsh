82aaccd5be165423b77aa7d2aa239354874501d8
Brandon R. Stoner <monokrome@monokro.me>
2013-08-17 21:15:03 -0700

    Quote paths.
    Quote variable pathnames to prevent unexpected behavior with spaces or other odd characters.

--- plugins/jump/jump.plugin.zsh
+++ plugins/jump/jump.plugin.zsh
@@ -6,16 +6,17 @@
 # unmark FOO: delete a mark
 # marks: lists all marks
 #
-export MARKPATH=$HOME/.marks
+export MARKPATH="$HOME/.marks"
+
 function jump { 
-	cd -P $MARKPATH/$1 2>/dev/null || echo "No such mark: $1"
+	cd -P "$MARKPATH/$1" 2>/dev/null || echo "No such mark: $1"
 }
 function mark { 
-	mkdir -p $MARKPATH; ln -s $(pwd) $MARKPATH/$1
+	mkdir -p "$MARKPATH"; ln -s "$(pwd)" "$MARKPATH/$1"
 }
 function unmark { 
-	rm -i $MARKPATH/$1 
+	rm -i "$MARKPATH/$1"
 }
 function marks {
-	ls -l $MARKPATH | sed 's/  / /g' | cut -d' ' -f9- | sed 's/ -/\t-/g' && echo
+	ls -l "$MARKPATH" | sed 's/  / /g' | cut -d' ' -f9- | sed 's/ -/\t-/g' && echo
 }
